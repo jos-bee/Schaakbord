1258324171ed1ffa14eeb5a666582e694b63d6da
Jos van Well <jos.van.well.bee@outlook.com>
2022-10-06 14:49:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hallo, Schaakbord!");
+Console.ReadLine();
+Console.Write("geef de hoogte van het bord ");
+int Hoogte = Convert.ToInt32(Console.ReadLine());
+Console.Write("Geef de breedte van het bord ");
+int Breedte = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("schaakbord word " + Hoogte + " hoog en " + Breedte + " breed");
+
+
+for (int h = 0; h < Hoogte; h++)
+{
+    for (int b = 0; b < Breedte; b++)
+    {
+        if ((b % 2 == 0) ^ (h % 2 == 0))
+            Console.Write("Z ");
+        else
+            Console.Write("W ");
+    }
+
+    Console.WriteLine();
+}
+
+
+Console.ReadLine();
